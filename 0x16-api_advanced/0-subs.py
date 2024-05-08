8135ecdb9974d45df540faf76f0628702fd399a4
Bpaul311 <bizimapaul777@gmail.com>
2024-05-08 06:53:59 -0700

    corrected everything

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Script that returns number of subs of a given subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subs = data['data']['subscribers']
-        return subs
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
