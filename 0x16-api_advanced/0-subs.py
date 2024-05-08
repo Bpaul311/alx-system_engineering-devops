0d66c7a143696564c92cb302d40fe7dd2b13fedc
Bpaul311 <bizimapaul777@gmail.com>
2024-05-08 06:46:57 -0700

    corrected the name

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "RedditScaper"}
+    headers = {"User-Agent": "RedditScraper"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
