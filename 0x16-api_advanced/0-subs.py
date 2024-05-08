ce42d60ac53a9d8b3df301fc6115bff49a8c6819
Bpaul311 <bizimapaul777@gmail.com>
2024-05-08 03:54:31 -0700

    fixing betty issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,17 @@
 """
 Script that returns number of subs of a given subreddit
 """
-import requests 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "RedditScaper"}
-    response = requests.get(url, headers= headers, allow_redirects = False)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        user_data = response.json()
-        subs = user['user_data']['subscribers']
+        data = response.json()
+        subs = data['data']['subscribers']
         return subs
     else:
         return 0
